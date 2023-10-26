93c043cf5deb94139b52e0b64338206877aba41b
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-26 15:34:12 +0300

    not func count app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    // want to make 3 week schedule that is clickable and be able to store data on what week is clicked and what day is clicked then be able to display that data on a different page
+    <div>
+      <div>
+        <h3>week 1</h3>
+        <div>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
