952f5b5539ec120e1b81737cedc99d8152531f2e
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-26 15:35:21 +0300

    clean up for useContext test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 export default function App() {
-  return (
-    // want to make 3 week schedule that is clickable and be able to store data on what week is clicked and what day is clicked then be able to display that data on a different page
+  return ( 
     <div>
       <div>
         <h3>week 1</h3>
